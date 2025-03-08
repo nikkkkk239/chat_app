73d08a617e9c8e0bb21d6f9f5a899edbe78b09e3
nikkkkk239 <nikhilpanday239@gmail.com>
2025-03-08 09:02:21 +0530

    Updated for deployment.

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,18 @@
 // const express = require("express")
 import express from "express";
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from './routes/message.route.js'
+import authRoutes from "./src/routes/auth.route.js"
+import messageRoutes from './src/routes/message.route.js'
 import cors from "cors"
 import dotenv from 'dotenv'
-import { connectDb } from "./lib/db.js";
-import {app,server} from "./lib/socket.js"
+import { connectDb } from "./src/lib/db.js";
+import {app,server} from "./src/lib/socket.js"
 import cookieParser from "cookie-parser";
-import path from "path"
 dotenv.config();
-const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 8000
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:process.env.FRONTEND_URL,
     credentials:true,
 }))
 app.use(express.json())
@@ -24,13 +22,6 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
-if(process.env.NODE_ENV == "production" ){
-    app.use(express.static(path.join(__dirname , "../frontend/dist")));
-
-    app.get("*" , (req,res)=>{
-        res.sendFile(path.join(__dirname , "../frontend" , "dist","index.html"));
-    })
-}
 server.listen(PORT,()=>{
     console.log(`server started at ${PORT}.` );
     connectDb();
